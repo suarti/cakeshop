67a22f5d194146cb415332ba7b08c598d4903c95
suarti <suarti35@gmail.com>
2021-06-27 20:21:24 +0700

    One commit

---
+++ resources/views/laporan.blade.php
@@ -0,0 +1,47 @@
+@extends('layouts.app2')
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8 mt-2">
+            <nav aria-label="breadcrumb">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="{{ url('admin') }}">Birtday Cake</a></li>
+                    <li class="breadcrumb-item active" aria-current="page">Laporan</li>
+                    <li class="breadcrumb-item"><a href="{{ url('admin-user') }}">Data User</a></li>
+                </ol>
+            </nav>
+        </div>
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-body">
+                    <h3><i class="fa fa-history"></i> Laporan Pendapatan ADMIN</h3>
+                    <table class="table table-striped">
+                        <thead>
+                            <tr>
+                                <th>No</th>
+                                <th>Tanggal</th>
+                                <th>Pendapatan (Rp.)</th>
+                                <th>Aksi</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php $no = 1; ?>
+                            @foreach($pesanans as $pesanan)
+                            <tr>
+                                <td>{{ $no++ }}</td>
+                                <td>{{ $pesanan->tanggal }}</td>
+                                <td>Rp. {{ number_format($pesanan->jumlah_harga+$pesanan->kode) }}</td>
+                                <td>
+                                <a href="/hapus/{{ $pesanan->id }}">Hapus</a>
+                                </td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>
+@endsection
